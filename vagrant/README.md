b52f30905b1ea3f1c8fb2bca7889b26876f5e50d
codecounselor <codecounselor@gmail.com>
2016-01-05 16:56:51 -0500

    Start of a vagrant configuration

---
+++ vagrant/README.md
@@ -0,0 +1,34 @@
+## Vagrant Setup
+
+You must install two programs which will automatically provision a development environment suitable for running and developing all Feature Guardian modules.
+
+#### Install the following:
+
+1. [Oracle VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+2. [Vagrant](https://www.vagrantup.com/downloads.html)
+
+#### Run Vagrant
+Note: The default config assumes you are running vagrant from inside a projects folder containing this repository, if that is not the case you must edit or comment out this line first in the file `Vagrantfile`:
+
+```
+config.vm.synced_folder "../../", "/projects"
+```
+
+This allows you to work on the host system and have all file changes automatically synced to the guest VM.
+
+Then to provision and start, run:
+
+```
+vagrant up
+```
+
+This will run an Ubuntu 14.04 LTS VM configured with 1GB RAM.  It installs the following for you:
+
+1. Node.JS version 4.2.x, and updates to the latest version of NPM
+2. Installs MongoDB version 3.2.x
+3. Installs Git and Git Flow
+4. Installs global npm modules:
+    * Gulp
+
+Once it starts you can execute `vagrant ssh` to gain access to the machine.
+
---
+++ vagrant/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Alias for Apt-Get installs to stay DRY
+install="sudo apt-get install -y"
+
+# Add the apt-get repo for node 4.2.x (LTS) and install it
+nodeVersion=`node --version`
+if [[ $nodeVersion != v4.2* ]] ; then 
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  sudo apt-get update
+  $install nodejs
+  # Get the latest version of NPM
+  sudo npm update -g npm
+fi
+
+# Install Mongo (latest version)
+mongoVersion="3.2.0"
+if [[  `which mongo` == "" ]] ; then
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+  echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+  sudo apt-get update
+  #$install --force-yes mongodb-org
+  $install mongodb-org=$mongoVersion \
+  mongodb-org-server=$mongoVersion \
+  mongodb-org-shell=$mongoVersion \
+  mongodb-org-mongos=$mongoVersion \
+  mongodb-org-tools=$mongoVersion
+fi
+
+# Install Git
+$install git
+$install git-flow
+
+# Install NPM Packages
+npm install -g gulp
+
+# Heat up the workspace
+cd /projects
+git clone https://github.com/featureguardian/fg-service.git
+git clone https://github.com/featureguardian/fg-admin-ui.git
+
+cd /projects/fg-service
+npm install
+
+cd /projects/fg-admin-ui
+npm install
